d6552c0061bc4c18e37a5c1fd7ada1b9885176c0
ShreyaCoditas <shreya.amanaganti@coditas.com>
2025-12-24 17:57:39 +0530

    swagger changes

--- src/main/java/com/example/accelerator/dto/QuestionConditionResponseDTO.java
+++ src/main/java/com/example/accelerator/dto/QuestionConditionResponseDTO.java
@@ -2,10 +2,14 @@ package com.example.accelerator.dto;
 
 import com.example.accelerator.domain.enums.ConditionOperator;
 import com.example.accelerator.domain.enums.ExpectedValue;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class QuestionConditionResponseDTO {
 
